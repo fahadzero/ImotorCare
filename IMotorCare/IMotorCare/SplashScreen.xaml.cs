530cca39dfb61eb0b39b63494b435bad8e39b219
tuzajohn <johntuza94@gmail.com>
2018-12-29 20:15:46 +0300

    added splashscreen

--- IMotorCare/IMotorCare/SplashScreen.xaml.cs
+++ IMotorCare/IMotorCare/SplashScreen.xaml.cs
@@ -19,13 +19,5 @@ namespace IMotorCare
             AbsoluteLayout.SetLayoutFlags(SplashScreenImage, AbsoluteLayoutFlags.PositionProportional);
             AbsoluteLayout.SetLayoutBounds(SplashScreenImage, new Rectangle(0.5, 0.5, AbsoluteLayout.AutoSize, AbsoluteLayout.AutoSize));
         }
-        protected override void OnAppearing()
-        {
-            base.OnAppearing();
-            SplashScreenImage.ScaleTo(1, 200);
-            SplashScreenImage.ScaleTo(0.9, 1500, Easing.Linear);
-            SplashScreenImage.ScaleTo(150, 1200, Easing.Linear);
-            Application.Current.MainPage = new NavigationPage(new MainPage());
-        }
     }
 }
